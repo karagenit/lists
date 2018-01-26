562d87eecab24d213b311c59e30a221d936e555a
Caleb Smith <karagenit@outlook.com>
2018-01-26 10:37:54 -0500

    Add Classes

--- script.rb
+++ script.rb
@@ -2,42 +2,37 @@
 
 require 'json'
 
-file = ARGV[0]
+class List
+  attr_reader :data
 
-data = []
-data = JSON.parse(IO.read(file)) unless file.nil?
+  def initialize(filename = nil)
+    @data = JSON.parse(IO.read(filename)) unless filename.nil?
+    @data |= { tags: [], items: [] }
+  end
 
-def list(data)
-  data.each_with_index do |elem, index|
-    puts "#{index}. #{elem[:name]}"
+  # Should be able to push/pop/delete/etc
+  def items(tag = nil)
+    if tag.nil?
+      @data[:items]
+    else
+      @data[:items].each { |item| item if item.tags.include? tag }
+    end
+  end
+
+  def tags
+    @data[:tags]
   end
 end
 
-def list_new(data)
-  print "Name: "
-  name = gets.chomp
-  data.push({ name: name })
+class Item
+  attr_reader :tags
+
+  def initialize(*tags)
+    @tags = tags
+  end
 end
 
-def list_edit(data)
-end
-
-def list_edit_name(data, number)
-end
-
-def list_add_tag(data, number)
-end
-
-def list_delete_tag(data, number)
-end
-
-def list_exit(data)
-  print "Output name (blank to overwrite): "
-  name = gets.chomp
-  name = file if name.empty? # this breaks
-  IO.write(name, data.to_json)
-  exit
-end
+list = List.new(ARGV[0])
 
 puts "Ready"
 
@@ -45,13 +40,9 @@ while true
   print "> "
   case gets.chomp
   when "list"
-    list(data)
   when "new"
-    list_new(data)
   when "edit"
-    list_edit(data)
   when "exit"
-    list_exit(data)
   else
     #print help
   end
